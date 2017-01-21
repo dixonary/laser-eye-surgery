8e6e18f410c166e0b946f6c5ca814637d8af79f6
dixonary <alexbrainbox@gmail.com>
2017-01-21 00:28:13 +0000

    Add MidiUser script demonstrating midi control

---
+++ Assets/MidiUser.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MidiJack;
+
+public class MidiUser : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Debug.Log(MidiMaster.GetKnob(21));
+	}
+}
