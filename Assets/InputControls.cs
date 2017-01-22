1f43bb02756d97f2f29c52cd7612b67024aab953
dixonary <alexbrainbox@gmail.com>
2017-01-22 07:26:14 +0000

    eye track notification?

---
+++ Assets/InputControls.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputControls : MonoBehaviour {
+
+    private static InputMode _inputMode;
+    public static InputMode inputMode {
+        get {
+            if (inputMode == InputMode.UNKNOWN) {
+                var res = Tobii.EyeTracking.EyeTrackingHost.GetInstance().EyeTrackingDeviceStatus;
+                if (res == Tobii.EyeTracking.DeviceStatus.Pending || res == Tobii.EyeTracking.DeviceStatus.Tracking) {
+                    _inputMode = InputMode.EYES;
+                }
+                else _inputMode = InputMode.MOUSE;
+            }
+            return _inputMode;
+        }
+    }
+
+	// Use this for initialization
+	void Start () {
+        _inputMode = InputMode.UNKNOWN;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
+
+public enum InputMode { MOUSE, EYES, UNKNOWN }
