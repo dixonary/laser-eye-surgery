b296492a2dd550caecb4ae4e8f62291d75795297
Will Tonna <Will@Wills-MacBook-Pro.local>
2017-01-22 16:59:10 +0000

    FINAL PUSH

--- Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
+++ Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -13,11 +13,41 @@ namespace AK
 {
     namespace EVENTS
     {
+        static const AkUniqueID CONVOFF = 2665135698U;
+        static const AkUniqueID CONVON = 4104583540U;
+        static const AkUniqueID DING = 2396968165U;
+        static const AkUniqueID ENDMUSIC = 1681803181U;
+        static const AkUniqueID FLESHOFF = 2613220256U;
+        static const AkUniqueID FLESHON = 146280682U;
+        static const AkUniqueID LASEROFF = 763008157U;
         static const AkUniqueID LASERON = 3636387825U;
+        static const AkUniqueID POP = 1081563622U;
+        static const AkUniqueID STARTAMBIENCE = 1316191055U;
+        static const AkUniqueID TITLEMUSIC = 2191159630U;
     } // namespace EVENTS
+
+    namespace STATES
+    {
+        namespace MUSICSTATES
+        {
+            static const AkUniqueID GROUP = 3103015060U;
+
+            namespace STATE
+            {
+                static const AkUniqueID FINAL = 565529991U;
+                static const AkUniqueID FIRST = 998496889U;
+                static const AkUniqueID SECOND = 3476314365U;
+                static const AkUniqueID THIRD = 931160808U;
+            } // namespace STATE
+        } // namespace MUSICSTATES
+
+    } // namespace STATES
 
     namespace GAME_PARAMETERS
     {
+        static const AkUniqueID COMBO = 2333396925U;
+        static const AkUniqueID COMPLETEDLIMBS = 1389571455U;
+        static const AkUniqueID PROGRESS = 308635872U;
         static const AkUniqueID SS_AIR_FEAR = 1351367891U;
         static const AkUniqueID SS_AIR_FREEFALL = 3002758120U;
         static const AkUniqueID SS_AIR_FURY = 1029930033U;
@@ -40,6 +70,8 @@ namespace AK
     {
         static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
         static const AkUniqueID MASTER_SECONDARY_BUS = 805203703U;
+        static const AkUniqueID MUSICBUS = 2886307548U;
+        static const AkUniqueID SFXBUS = 3803850708U;
     } // namespace BUSSES
 
 }// namespace AK
