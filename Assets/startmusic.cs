2053288ea53e42e3fc6946fc1aea2b9ffd64047b
dixonary <alexbrainbox@gmail.com>
2017-01-23 18:13:19 +0000

    added missing files; wwise linux support

---
+++ Assets/startmusic.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class startmusic : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        //AkSoundEngine.PostEvent("TitleMusic", gameObject);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+    }
+}
---
+++ Assets/valset.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class valset : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        gameObject.GetComponent<Text>().text = GameController._completed.ToString();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
